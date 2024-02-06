11aa676a304082551749b9eb8e318ece22f1e359
Sumeet Bandgar <sumeet.bandgar@verinite.com>
2024-02-06 18:20:49 +0530

    Added cors header

--- src/main/java/com/verinite/commons/controller/LookupController.java
+++ src/main/java/com/verinite/commons/controller/LookupController.java
@@ -6,6 +6,7 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.verinite.commons.dto.StatusResponse;
 import com.verinite.commons.service.LookupService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/lookup")
 public class LookupController {

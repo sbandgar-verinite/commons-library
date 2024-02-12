a73aced75e09106eb736a2c8c3ee099c13baf8b7
Sumeet Bandgar <sumeet.bandgar@verinite.com>
2024-02-12 18:41:18 +0530

    Added redis config

--- src/main/java/com/verinite/commons/CommonsApplication.java
+++ src/main/java/com/verinite/commons/CommonsApplication.java
@@ -2,8 +2,10 @@ package com.verinite.commons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CommonsApplication {
 
 	public static void main(String[] args) {
